2baa13817cd5348ca34a15074419921e99078a26
Luca Del Conte <delconteluca55@gmail.com>
2024-10-27 15:36:00 +0100

    Update

--- Broken Origami Compaq (Missing Texture & Death Anim)/OrigamiCrowd.lua
+++ Broken Origami Compaq (Missing Texture & Death Anim)/OrigamiCrowd.lua
@@ -1,29 +1,12 @@
 local game = Game()
 local OrigamiCrowdLocalID = Isaac.GetItemIdByName("Origami Crowd")
-local lastPassiveItemID = nil
+local chargeMemory = {}
 
 if EID then
     EID:assignTransformation("collectible", OrigamiCrowdLocalID, EID.TRANSFORMATION["ORIGAMI"])
-    EID:addCollectible(OrigamiCrowdLocalID, "Increase every item stats up:#{{ArrowUp}} Damage +0.3#{{ArrowUp}} Speed +0.1#{{ArrowUp}} Range +0.5#{{ArrowUp}} Tears +0.3#{{ArrowUp}} Luck +0.2")
+    EID:addCollectible(OrigamiCrowdLocalID, "Duplicate all charge of active item")
 end
 
--- Valori base per l'aumento delle statistiche
-local statMultiplier = {
-    damage = 0.3,  -- +0.3 Damage per ogni broken heart
-    speed = 0.1,   -- +0.1 Speed per ogni broken heart
-    range = 20,    -- +0.5 Range per ogni broken heart
-    tears = 0.3,   -- +0.3 Fire Rate per ogni broken heart
-    luck = 0.2,    -- +0.2 Luck per ogni broken heart
-}
-
--- Variabile per tenere traccia del numero di broken hearts
-local flagCounter = {
-    damage = 0,
-    speed = 0,
-    range = 0,
-    tears = 0,
-    luck = 0,
-}
 
 function BrokenOrigami:useOrigamiCrowd(player)
     -- Get the player's data table
@@ -45,9 +28,28 @@ function BrokenOrigami:useOrigamiCrowd(player)
         if data.OrigamiCrowdCounter >= data.OrigamiCrowdPreviousCounter then
             data.OrigamiCrowdPreviousCounter = data.OrigamiCrowdPreviousCounter + 1
             data.OrigamiCrowdRelative = data.OrigamiCrowdRelative + 1
-            player:AddBrokenHearts(1) -- Add 1 broken heart
-            lastPassiveItemID = OrigamiCrowdLocalID
+            player:AddBrokenHearts(2) -- Add 2 broken heart
         end
+
+        for i = 0, 3 do -- Check all active item slots
+            local activeItem = player:GetActiveItem(i)
+            if activeItem ~= 0 then
+                local currentCharge = player:GetActiveCharge(i)
+                
+                -- Retrieve the previous charge or set it to the current charge if first time
+                local previousCharge = chargeMemory[i] or currentCharge
+                chargeMemory[i] = currentCharge -- Update previous charge for the next cycle
+                
+                -- Calculate the charge difference
+                local chargeGained = currentCharge - previousCharge
+                
+                -- Double only the new charge gained
+                if chargeGained > 0 then
+                    player:SetActiveCharge(currentCharge + chargeGained, i) -- Double new charge only
+                end
+            end
+        end
+
     else
         data.OrigamiCrowdCounter = 0
         data.OrigamiCrowdPreviousCounter = 1
@@ -57,47 +59,6 @@ function BrokenOrigami:useOrigamiCrowd(player)
     end
 end
 
-function BrokenOrigami:onEvaluateCacheOrigamiCrowd(player, cacheFlag)
-    local data = player:GetData()
-    local itemConfig = Isaac.GetItemConfig():GetCollectible(collectibleID)
-
-    -- Controlla se l'oggetto raccolto è passivo
-    if itemConfig and itemConfig.Type ~= ItemType.ITEM_ACTIVE and not itemConfig == OrigamiCrowdLocalID then
-        -- Salva l'ID dell'ultimo oggetto passivo raccolto
-        lastPassiveItemID = collectibleID
-    end
-
-    if player:HasCollectible(OrigamiCrowdLocalID) and itemConfig then
-        if cacheFlag == CacheFlag.CACHE_DAMAGE then
-            flagCounter.damage = flagCounter.damage + 1
-            player.Damage = player.Damage + (flagCounter.damage * statMultiplier.damage)
-        elseif cacheFlag == CacheFlag.CACHE_SPEED then
-            flagCounter.speed = flagCounter.speed + 1
-            player.MoveSpeed = player.MoveSpeed + (flagCounter.speed * statMultiplier.speed)
-        elseif cacheFlag == CacheFlag.CACHE_RANGE then
-            flagCounter.range = flagCounter.range + 1
-            player.TearRange = player.TearRange + (flagCounter.range * statMultiplier.range)
-        elseif cacheFlag == CacheFlag.CACHE_FIREDELAY then
-            flagCounter.tears = flagCounter.tears + 1
-            local newTears = (30.0 / (player.MaxFireDelay + 1)) + (flagCounter.tears * statMultiplier.tears)
-            player.MaxFireDelay = (30.0 / newTears) - 1
-        elseif cacheFlag == CacheFlag.CACHE_LUCK then
-            flagCounter.luck = flagCounter.luck + 1
-            player.Luck = player.Luck + (flagCounter.luck * statMultiplier.luck)
-        end
-    end
-end
-
-function BrokenOrigami:onGameStartOrigamiCrowd()
-    flagCounter.damage = 0
-    flagCounter.speed = 0
-    flagCounter.range = 0
-    flagCounter.tears = 0
-    flagCounter.luck = 0
-    lastPassiveItemID = nil
-end
-
 
 BrokenOrigami:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, BrokenOrigami.useOrigamiCrowd)
-BrokenOrigami:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, BrokenOrigami.onEvaluateCacheOrigamiCrowd)
-BrokenOrigami:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, BrokenOrigami.onGameStartOrigamiCrowd)
+
