78541c41764a360ec601d94843148be3d2d04733
Luca Del Conte <delconteluca55@gmail.com>
2024-11-05 18:22:37 +0100

    Balancing

--- Broken Origami Compaq (Missing Texture & Death Anim)/Girlfriend.lua
+++ Broken Origami Compaq (Missing Texture & Death Anim)/Girlfriend.lua
@@ -3,7 +3,7 @@ local GirlfriendLocalID = Isaac.GetItemIdByName("Girlfriend")
 
 -- EID (External Item Descriptions)
 if EID then
-    EID:addCollectible(GirlfriendLocalID, "{{ArrowUp}} Give a random familiar #{{ArrowDown}} Give 1 Broken Heart {{BrokenHeart}} that replace Heart in this order {{Heart}}{{BoneHeart}}{{SoulHeart}}{{BlackHeart}}")
+    EID:addCollectible(GirlfriendLocalID, "{{ArrowUp}} Give a random familiar #{{ArrowDown}} Give 1 Broken Heart {{BrokenHeart}} that replace Heart in this order {{Heart}}{{BoneHeart}}{{SoulHeart}}{{BlackHeart}} #{{Player10}} Give 5 Whisp then item disappear #{{Player31}} Give 7 Whisp then item disappear")
 end
 
 local familiars = {}
@@ -23,33 +23,51 @@ local function onGameStartGirlfriend()
 end
 
 function BrokenOrigami:useGirlfriend(_, rng, player)
+    local playerType = player:GetPlayerType()
     if player:HasCollectible(GirlfriendLocalID) then
         local familiarID = familiars[rng:RandomInt(#familiars) + 1]
         
-        local slotRemoved = false
+        if playerType == PlayerType.PLAYER_THELOST or playerType == PlayerType.PLAYER_THELOST_B then
+            player:AddWisp(GirlfriendLocalID, player.Position)
+            player:AddWisp(GirlfriendLocalID, player.Position)
+            player:AddWisp(GirlfriendLocalID, player.Position)
+            player:AddWisp(GirlfriendLocalID, player.Position)
+            player:AddWisp(GirlfriendLocalID, player.Position)
+            if playerType == PlayerType.PLAYER_THELOST_B then
+                player:AddWisp(GirlfriendLocalID, player.Position)
+                player:AddWisp(GirlfriendLocalID, player.Position)
+            end  
+            return {
+                Discharge = true,
+                Remove = true,
+                ShowAnim = true
+            }
+        else
+            local slotRemoved = false
 
-        if player:GetMaxHearts() >= 2 and not slotRemoved then
-            player:AddMaxHearts(-2)  -- Rimuove mezzo cuore rosso
-            slotRemoved = true
+            if player:GetMaxHearts() >= 2 and not slotRemoved then
+                player:AddMaxHearts(-2)  -- Rimuove mezzo cuore rosso
+                slotRemoved = true
+            end
+
+            if not slotRemoved and player:GetBoneHearts() >= 1 then
+                player:AddBoneHearts(-1)  -- Rimuove un cuore osso intero
+                slotRemoved = true
+            end
+
+            if not slotRemoved and player:GetSoulHearts() >= 2 then
+                player:AddSoulHearts(-2)  -- Rimuove mezzo cuore dell'anima
+                slotRemoved = true
+            end
+
+            if not slotRemoved and player:GetBlackHearts() >= 2 then
+                player:AddBlackHearts(-2)  -- Rimuove mezzo cuore nero
+                slotRemoved = true
+            end
+
+            player:AddBrokenHearts(1)
+            player:AddCollectible(familiarID)
         end
-
-        if not slotRemoved and player:GetBoneHearts() >= 1 then
-            player:AddBoneHearts(-1)  -- Rimuove un cuore osso intero
-            slotRemoved = true
-        end
-
-        if not slotRemoved and player:GetSoulHearts() >= 2 then
-            player:AddSoulHearts(-2)  -- Rimuove mezzo cuore dell'anima
-            slotRemoved = true
-        end
-
-        if not slotRemoved and player:GetBlackHearts() >= 2 then
-            player:AddBlackHearts(-2)  -- Rimuove mezzo cuore nero
-            slotRemoved = true
-        end
-
-        player:AddBrokenHearts(1)
-        player:AddCollectible(familiarID)
     end
 
     return {
