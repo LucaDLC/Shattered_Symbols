2e264bb7a3d0800a591850db0c6f3c1d789aa114
Luca Del Conte <delconteluca55@gmail.com>
2024-11-06 01:05:13 +0100

    Sacred Lantern is now Active Item

--- Broken Origami Compaq (Missing Texture & Death Anim)/SacredLantern.lua
+++ Broken Origami Compaq (Missing Texture & Death Anim)/SacredLantern.lua
@@ -7,43 +7,27 @@ if EID then
 end
 
 -- Function to handle item pickup
-function BrokenOrigami:useSacredLantern(player)
-    local data = player:GetData()
-    
-    -- Initialize the SacredLanternCounter if it doesn't exist
-    if not data.SacredLanternCounter then
-        data.SacredLanternCounter = 0
-        data.SacredLanternRelative = 0
-        data.SacredLanternPreviousCounter = 1
-    end
+function BrokenOrigami:useSacredLantern(_, rng, player)
 
-    -- Check if the player has picked up the item
     if player:HasCollectible(SacredLanternLocalID) then
-        -- Increase the counter
-        data.SacredLanternCounter = player:GetCollectibleNum(SacredLanternLocalID)
-        
-        -- Apply the effect based on the number of items picked up
-        if data.SacredLanternCounter >= data.SacredLanternPreviousCounter then
-            data.SacredLanternPreviousCounter = data.SacredLanternPreviousCounter + 1
-            data.SacredLanternRelative = data.SacredLanternRelative + 1
-            local brokenHearts = player:GetBrokenHearts() -- Get number of broken hearts
-            if brokenHearts > 0 then
-                -- For each broken heart, add a half soul heart and remove one broken heart
-                for i = 1, brokenHearts do
-                    player:AddSoulHearts(1) -- Adds half a soul heart
-                    player:AddBrokenHearts(-1) -- Removes one broken heart
-                end
+        local brokenHearts = player:GetBrokenHearts() -- Get number of broken hearts
+        if brokenHearts > 0 then
+            -- For each broken heart, add a half soul heart and remove one broken heart
+            for i = 1, brokenHearts do
+                player:AddSoulHearts(1) -- Adds half a soul heart
+                player:AddBrokenHearts(-1) -- Removes one broken heart
             end
         end
-    else
-        data.SacredLanternCounter = 0
-        data.SacredLanternPreviousCounter = 1
+        
     end
-    if data.SacredLanternRelative > data.SacredLanternCounter then
-        data.SacredLanternPreviousCounter = data.SacredLanternCounter +1
-    end
+
+    return {
+        Discharge = true,
+        Remove = true,
+        ShowAnim = true
+    }
     
 end
 
 
-BrokenOrigami:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, BrokenOrigami.useSacredLantern)
+BrokenOrigami:AddCallback(ModCallbacks.MC_USE_ITEM, BrokenOrigami.useSacredLantern, SacredLanternLocalID)
