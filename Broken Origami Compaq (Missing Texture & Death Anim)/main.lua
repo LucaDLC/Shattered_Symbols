9bed7667b57abaf75e63b1808c4b85af54805b5a
Luca Del Conte <delconteluca55@gmail.com>
2024-11-08 01:14:15 +0100

    New Attempt

--- Broken Origami Compaq (Missing Texture & Death Anim)/main.lua
+++ Broken Origami Compaq (Missing Texture & Death Anim)/main.lua
@@ -21,26 +21,85 @@ local ItemScript = {
 }
 
 for Load = 1, #ItemScript do
-	require("script.item." .. ItemScript[Load])
+    require("script.item." .. ItemScript[Load])
 end
 
 
 function BrokenOrigami:SavePlayerData()
     local player = Isaac.GetPlayer(0)
-    AllPlayerDataToSave = player:GetData()
-    BrokenOrigami:SaveData(JsonSaveFile.encode(AllPlayerDataToSave)) 
+    local data = player:GetData()
+
+    -- Salva tutte le variabili del giocatore
+    AllPlayerDataToSave = {
+        OrigamiSwanRelative = data.OrigamiSwanRelative or 0,
+        OrigamiSwanPreviousCounter = data.OrigamiSwanPreviousCounter or 0,
+        OrigamiShurikenRelative = data.OrigamiShurikenRelative or 0,
+        OrigamiShurikenPreviousCounter = data.OrigamiShurikenPreviousCounter or 0,
+        OrigamiShurikenDamageBoost = data.OrigamiShurikenDamageBoost or 0,
+        OrigamiKolibriRelative = data.OrigamiKolibriRelative or 0,
+        OrigamiKolibriPreviousCounter = data.OrigamiKolibriPreviousCounter or 0,
+        kolibriTearsCount = data.kolibriTearsCount or 0,
+        OrigamiKolibriSpeedBoost = data.OrigamiKolibriSpeedBoost or 0,
+        OrigamiKolibriLimit = data.OrigamiKolibriLimit or 0,
+        chargeMemory = data.chargeMemory or 0,
+        OrigamiCrowRelative = data.OrigamiCrowRelative or 0,
+        OrigamiCrowPreviousCounter = data.OrigamiCrowPreviousCounter or 0,
+        brokenHeartsCount = data.brokenHeartsCount or 0,
+        holdingItemforStats = data.holdingItemforStats or 0,
+        MeteorRelative = data.MeteorRelative or 0,
+        MeteorPreviousCounter = data.MeteorPreviousCounter or 0,
+        FortuneTellerRelative = data.FortuneTellerRelative or 0,
+        FortuneTellerPreviousCounter = data.FortuneTellerPreviousCounter or 0,
+        FortuneTellerLuckBoost = data.FortuneTellerLuckBoost or 0,
+        BrokenFluxPreviousBrokenHearts = data.BrokenFluxPreviousBrokenHearts or 0,
+        BrokenFluxCharge = data.BrokenFluxCharge or 0
+    }
+
+    BrokenOrigami:SaveData(JsonSaveFile.encode(AllPlayerDataToSave))
 end
 
-
+-- Funzione di caricamento dei dati
 function BrokenOrigami:LoadPlayerData()
+    local player = Isaac.GetPlayer(0)
+    local data = player:GetData()
+
     if BrokenOrigami:HasData() then
-        local player = Isaac.GetPlayer(0)
+        -- Carica i dati solo se sono presenti nel file
         AllPlayerDataToSave = JsonSaveFile.decode(BrokenOrigami:LoadData())
-        
-        for key, value in pairs(AllPlayerDataToSave) do
-            player:GetData()[key] = value
-        end
+
+        -- Assegna le variabili salvate al giocatore
+        data.OrigamiSwanRelative = AllPlayerDataToSave.OrigamiSwanRelative or 0
+        data.OrigamiSwanPreviousCounter = AllPlayerDataToSave.OrigamiSwanPreviousCounter or 0
+        data.OrigamiShurikenRelative = AllPlayerDataToSave.OrigamiShurikenRelative or 0
+        data.OrigamiShurikenPreviousCounter = AllPlayerDataToSave.OrigamiShurikenPreviousCounter or 0
+        data.OrigamiShurikenDamageBoost = AllPlayerDataToSave.OrigamiShurikenDamageBoost or 0
+        data.OrigamiKolibriRelative = AllPlayerDataToSave.OrigamiKolibriRelative or 0
+        data.OrigamiKolibriPreviousCounter = AllPlayerDataToSave.OrigamiKolibriPreviousCounter or 0
+        data.kolibriTearsCount = AllPlayerDataToSave.kolibriTearsCount or 0
+        data.OrigamiKolibriSpeedBoost = AllPlayerDataToSave.OrigamiKolibriSpeedBoost or 0
+        data.OrigamiKolibriLimit = AllPlayerDataToSave.OrigamiKolibriLimit or 0
+        data.chargeMemory = AllPlayerDataToSave.chargeMemory or 0
+        data.OrigamiCrowRelative = AllPlayerDataToSave.OrigamiCrowRelative or 0
+        data.OrigamiCrowPreviousCounter = AllPlayerDataToSave.OrigamiCrowPreviousCounter or 0
+        data.brokenHeartsCount = AllPlayerDataToSave.brokenHeartsCount or 0
+        data.holdingItemforStats = AllPlayerDataToSave.holdingItemforStats or 0
+        data.MeteorRelative = AllPlayerDataToSave.MeteorRelative or 0
+        data.MeteorPreviousCounter = AllPlayerDataToSave.MeteorPreviousCounter or 0
+        data.FortuneTellerRelative = AllPlayerDataToSave.FortuneTellerRelative or 0
+        data.FortuneTellerPreviousCounter = AllPlayerDataToSave.FortuneTellerPreviousCounter or 0
+        data.FortuneTellerLuckBoost = AllPlayerDataToSave.FortuneTellerLuckBoost or 0
+        data.BrokenFluxPreviousBrokenHearts = AllPlayerDataToSave.BrokenFluxPreviousBrokenHearts or 0
+        data.BrokenFluxCharge = AllPlayerDataToSave.BrokenFluxCharge or 0
+    else
+        -- Se non ci sono dati salvati, resetta i dati del giocatore
+        BrokenOrigami:EraseData()
     end
+end
+
+-- Funzione per cancellare i dati alla fine di una partita
+function BrokenOrigami:EraseDataOnNewGameOrEnd()
+    isNewGame = true  -- Reset flag
+    BrokenOrigami:SaveData("{}")  -- Cancella i dati salvati
 end
 
 function BrokenOrigami:ExecuteConsoleCommand(_, Command)
@@ -57,8 +116,12 @@ function BrokenOrigami:ExecuteConsoleCommand(_, Command)
     end
 end
 
---BrokenOrigami:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, BrokenOrigami.SavePlayerData)
+BrokenOrigami:AddCallback(ModCallbacks.MC_EXECUTE_CMD,BrokenOrigami.ExecuteConsoleCommand)
+
+BrokenOrigami:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, BrokenOrigami.SavePlayerData)
 
 --BrokenOrigami:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, BrokenOrigami.LoadPlayerData)
 
-BrokenOrigami:AddCallback(ModCallbacks.MC_EXECUTE_CMD,BrokenOrigami.ExecuteConsoleCommand)
+--BrokenOrigami:AddCallback(ModCallbacks.MC_POST_GAME_END, BrokenOrigami.EraseDataOnNewGameOrEnd)
+
+--BrokenOrigami:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, BrokenOrigami.EraseDataOnNewGameOrEnd)
