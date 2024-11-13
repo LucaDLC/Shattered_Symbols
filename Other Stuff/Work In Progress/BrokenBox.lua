5909893c8fd0b9d5a2e0c3dde81d12671588cd3c
Luca Del Conte <delconteluca55@gmail.com>
2024-11-13 22:51:51 +0100

    Created new item
    is not finish yet

---
+++ Other Stuff/Work In Progress/BrokenBox.lua
@@ -0,0 +1,79 @@
+local game = Game()
+local BrokenBoxLocalID = Isaac.GetItemIdByName("Broken Box")
+
+
+-- EID (External Item Descriptions)
+if EID then
+    EID:addCollectible(BrokenBoxLocalID, "{{Warning}} SINGLE USE {{Warning}} #If empty, the box takes up to maximum of: #{{BrokenHeart}} 1 Broken Heart #{{Collectible}} 1 Item #{{Coin}} 10 Coins #{{Bomb}} 1 Bomb #{{Key}} 1 Key #If full, the box gives you everything it took before")
+end
+
+
+-- Funzione per gestire l'uso dell'oggetto "Wriggling Shadow"
+function BrokenOrigami:useBrokenBox(_, rng, player)
+
+        if not data.BrokenBoxHeartFlag then data.BrokenBoxHeartFlag = false,
+        if not data.BrokenBoxItemFlag then data.BrokenBoxItemFlag = nil,
+        if not data.BrokenBoxMoneyFlag then data.BrokenBoxMoneyFlag = 0,
+        if not data.BrokenBoxBombFlag then data.BrokenBoxBombFlag = 0,
+        if not data.BrokenBoxKeyFlag then data.BrokenBoxKeyFlag = 0,
+        if not data.BrokenBoxStatus then data.BrokenBoxStatus = false
+
+    if player:HasCollectible(BrokenBoxLocalID) and not data.BrokenBoxStatus then
+
+        if player:GetBrokenHearts() > 0 then
+            player:AddBrokenHearts(-1)
+            data.BrokenBoxHeartFlag = true
+        end
+
+        if #player:GetCollectibles() > 0 then
+            data.BrokenBoxItemFlag = player:GetCollectibles()[rng:RandomInt(#player:GetCollectibles()) + 1]
+            player:RemoveCollectible(data.BrokenBoxItemFlag)
+        end
+
+        data.BrokenBoxMoneyFlag = math.min(player:GetNumCoins(), 10)
+        player:AddCoins(-data.BrokenBoxMoneyFlag)
+
+        data.BrokenBoxBombFlag = math.min(player:GetNumBombs(), 1)
+        player:AddBombs(-data.BrokenBoxBombFlag)
+
+        data.BrokenBoxKeyFlag = math.min(player:GetNumKeys(), 1)
+        player:AddKeys(-data.BrokenBoxKeyFlag)
+
+       
+    else if player:HasCollectible(BrokenBoxLocalID) and data.BrokenBoxStatus then
+
+        if data.BrokenBoxHeartFlag then
+            player:AddBrokenHearts(1)
+            data.BrokenBoxHeartFlag = false
+        end
+
+        if data.BrokenBoxItemFlag ~= nil then
+            player:AddCollectible(data.BrokenBoxItemFlag)
+            data.BrokenBoxItemFlag = nil
+        end
+
+        if data.BrokenBoxMoneyFlag > 0 then
+            player:AddCoins(data.BrokenBoxMoneyFlag)
+            data.BrokenBoxMoneyFlag = 0
+        end
+
+        if data.BrokenBoxBombFlag > 0 then
+            player:AddBombs(data.BrokenBoxBombFlag)
+            data.BrokenBoxBombFlag = 0
+        end
+
+        if data.BrokenBoxKeyFlag > 0 then
+            player:AddKeys(data.BrokenBoxKeyFlag)
+            data.BrokenBoxKeyFlag = 0
+        end
+        
+    end
+
+    return {
+        Discharge = true,
+        Remove = true,
+        ShowAnim = true
+    }
+end
+
+BrokenOrigami:AddCallback(ModCallbacks.MC_USE_ITEM, BrokenOrigami.useBrokenBox, BrokenBoxLocalID)
