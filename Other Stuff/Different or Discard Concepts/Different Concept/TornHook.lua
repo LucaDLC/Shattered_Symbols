1e9167869396a7bd5d06d28555e6829d1fc48219
Luca Del Conte <delconteluca55@gmail.com>
2024-11-22 00:27:02 +0100

    New Concept

---
+++ Other Stuff/Different or Discard Concepts/Different Concept/TornHook.lua
@@ -0,0 +1,32 @@
+
+local game = Game()
+local TornHookLocalID = Isaac.GetItemIdByName("Torn Hook")
+
+-- EID (se usi EID per la descrizione)
+if EID then
+    EID:assignTransformation("collectible", TornHookLocalID, EID.TRANSFORMATION["ORIGAMI"])
+    EID:addCollectible(TornHookLocalID, "{{BrokenHeart}} Gives 1 Broken Hearts at every Floor #Every floor you have 20% of chance (+10% for each Torn Hook after first) of spawn Death Certificate #Jacob and Esau have +10% chance and both have their own percent")
+end
+
+function BrokenOrigami:onTornHook()
+    for playerIndex = 0, game:GetNumPlayers() - 1 do
+        local player = Isaac.GetPlayer(playerIndex)
+        if player:HasCollectible(TornHookLocalID) then
+            local TornHooksCounter = player:GetCollectibleNum(TornHookLocalID)
+            player:AddBrokenHearts(1*TornHooksCounter)
+            local TornHookChance = 0
+            TornHookChance = 0.20 + ((TornHooksCounter - 1) * 0.10)
+            if player:GetPlayerType() == PlayerType.PLAYER_JACOB or player:GetPlayerType() == PlayerType.PLAYER_ESAU then
+                TornHookChance = TornHookChance + 0.10
+            end
+            if math.random() < TornHookChance then
+                Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, CollectibleType.COLLECTIBLE_DEATH_CERTIFICATE, player.Position + Vector(0, 50), Vector(0,0), nil)
+                for i = 1, player:GetCollectibleNum(TornHookExternalID) do
+                    player:RemoveCollectible(TornHookExternalID)
+                end
+            end
+        end
+    end
+end
+
+BrokenOrigami:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, BrokenOrigami.onTornHook)
---
+++ Other Stuff/Different or Discard Concepts/Different Concept/WrigglingShadow.lua
@@ -0,0 +1,48 @@
+local game = Game()
+local WrigglingShadowLocalID = Isaac.GetItemIdByName("Wriggling Shadow")
+local TornHookExternalID = Isaac.GetItemIdByName("Torn Hook")
+local AncientHookExternalID = Isaac.GetItemIdByName("Ancient Hook")
+
+-- EID (External Item Descriptions)
+if EID then
+    EID:addCollectible(WrigglingShadowLocalID, "{{Warning}} SINGLE USE {{Warning}} #{{ArrowUp}} Remove all Hooks, remove 1 Broken Heart and give 1 Full Heart for each Hook #{{ArrowDown}} If you don't have any hooks, it gives you one")
+end
+
+-- Funzione per gestire l'uso dell'oggetto "Wriggling Shadow"
+function BrokenOrigami:useWrigglingShadow(_, rng, player)
+    if (player:HasCollectible(TornHookExternalID) or player:HasCollectible(AncientHookExternalID)) and player:HasCollectible(WrigglingShadowLocalID) then
+
+        -- Rimuove tutti gli oggetti Torn Hook
+        for i = 1, player:GetCollectibleNum(TornHookExternalID) do
+            player:RemoveCollectible(TornHookExternalID)
+            player:AddBrokenHearts(-1)
+            player:AddMaxHearts(2)
+            player:AddHearts(2)
+        end
+
+        for i = 1, player:GetCollectibleNum(AncientHookExternalID) do
+            player:RemoveCollectible(AncientHookExternalID)
+            player:AddBrokenHearts(-1)
+            player:AddMaxHearts(2)
+            player:AddHearts(2)
+        end
+
+    elseif (not player:HasCollectible(TornHookExternalID) and not player:HasCollectible(AncientHookExternalID)) and player:HasCollectible(WrigglingShadowLocalID) then
+        local hookProb = math.random(0, 1)
+        if hookProb == 0 then
+            player:GiveCollectible(TornHookExternalID)
+        else
+            player:GiveCollectible(AncientHookExternalID)
+        end
+    end
+
+    -- Rimuove l'oggetto Wriggling Shadow dopo l'uso
+    return {
+        Discharge = true,
+        Remove = true,
+        ShowAnim = true
+    }
+end
+
+
+BrokenOrigami:AddCallback(ModCallbacks.MC_USE_ITEM, BrokenOrigami.useWrigglingShadow, WrigglingShadowLocalID)
