ec5312a297113bc0d4eb4a9c05ef5850af9c3d3b
Luca Del Conte <delconteluca55@gmail.com>
2024-12-16 00:04:42 +0100

    Improved Body

--- Broken Origami Compaq (Missing Texture & Death Anim)/script/item/ForbiddenBody.lua
+++ Broken Origami Compaq (Missing Texture & Death Anim)/script/item/ForbiddenBody.lua
@@ -6,7 +6,15 @@ if EID then
     EID:addCollectible(ForbiddenBodyLocalID, "{{EthernalHeart}} Half Eternal Heart count as 2 and can remove 1 Broken Heart")
 end
 
-function BrokenOrigami:useForbidenBody(pickup, collider)
+function BrokenOrigami:useForbidenBody(player)
+    if player:GetEternalHearts() == 1 and player:HasCollectible(ForbiddenBodyLocalID) then
+        player:AddBrokenHearts(-1) -- Rimuovi un broken heart
+        player:AddEternalHearts(1) -- Aggiungi un altro half Eternal Heart
+    end
+
+end
+
+function BrokenOrigami:holdingForbidenBody(pickup, collider)
     if collider:ToPlayer() then
         local player = collider:ToPlayer()
         if pickup.Variant == PickupVariant.PICKUP_HEART and pickup.SubType == HeartSubType.HEART_ETERNAL and player:HasCollectible(ForbiddenBodyLocalID) then
@@ -16,5 +24,5 @@ function BrokenOrigami:useForbidenBody(pickup, collider)
     end
 end
 
--- Registra il callback per verificare l'ingresso nella Devil Room
-BrokenOrigami:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, BrokenOrigami.useForbidenBody)
+BrokenOrigami:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, BrokenOrigami.useForbidenBody)
+BrokenOrigami:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, BrokenOrigami.holdingForbidenBody)
