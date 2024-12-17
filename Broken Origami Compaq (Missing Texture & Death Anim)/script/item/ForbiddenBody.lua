df4596c2d633dbed6b30eee2896c8dc01e1b1056
Luca Del Conte <delconteluca55@gmail.com>
2024-12-17 14:35:38 +0100

    Update Forbidden Body

--- Broken Origami Compaq (Missing Texture & Death Anim)/script/item/ForbiddenBody.lua
+++ Broken Origami Compaq (Missing Texture & Death Anim)/script/item/ForbiddenBody.lua
@@ -3,7 +3,7 @@ local ForbiddenBodyLocalID = Isaac.GetItemIdByName("Forbidden Body")
 
 -- EID (se usi EID per la descrizione)
 if EID then
-    EID:addCollectible(ForbiddenBodyLocalID, "{{EthernalHeart}} Half Eternal Heart count as 2 and can remove 1 Broken Heart")
+    EID:addCollectible(ForbiddenBodyLocalID, "{{EthernalHeart}} Half Eternal Heart count as 2 and can remove 1 Broken Heart #{{BossRoom}} When you enter in the Boss Room for the first time you have 20% to obtain an Half Eternal Heart")
 end
 
 function BrokenOrigami:useForbidenBody(player)
@@ -24,5 +24,19 @@ function BrokenOrigami:holdingForbidenBody(pickup, collider)
     end
 end
 
+function BrokenOrigami:onBossRoomForbidenBody()
+    local room = game:GetRoom()
+
+    for playerIndex = 0, game:GetNumPlayers() - 1 do
+        local player = Isaac.GetPlayer(playerIndex)
+        if room:GetType() == RoomType.ROOM_BOSS and room:IsFirstVisit() and player:HasCollectible(ForbiddenBodyLocalID) then
+            if math.random() <= 0.20 then 
+                player:AddEternalHearts(1) 
+            end
+        end
+    end
+end
+
 BrokenOrigami:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, BrokenOrigami.useForbidenBody)
 BrokenOrigami:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, BrokenOrigami.holdingForbidenBody)
+BrokenOrigami:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, BrokenOrigami.onBossRoomForbidenBody)
