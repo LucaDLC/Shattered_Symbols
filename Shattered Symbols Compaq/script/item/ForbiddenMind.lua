50210be5904bd6d4f9bfbe9b150b815987c6fe65
Luca Del Conte <delconteluca55@gmail.com>
2024-12-25 00:22:05 +0100

    Removed Red Treasure Feature

--- Shattered Symbols Compaq/script/item/ForbiddenMind.lua
+++ Shattered Symbols Compaq/script/item/ForbiddenMind.lua
@@ -3,7 +3,7 @@ local ForbiddenMindLocalID = Isaac.GetItemIdByName("Forbidden Mind")
 
 -- EID (se usi EID per la descrizione)
 if EID then
-    EID:addCollectible(ForbiddenMindLocalID, "{{Room}} After clearing a Room have 25% to open Red Rooms around it #{{TreasureRoom}} At every floor have 40% to trasform Treasure Room in Red Treasure Room")
+    EID:addCollectible(ForbiddenMindLocalID, "{{Room}} After clearing a Room have 25% to open Red Rooms around it")
 end
 
 function ShatteredSymbols:onRoomClearForbiddenMind()
@@ -24,18 +24,4 @@ function ShatteredSymbols:onRoomClearForbiddenMind()
 end
 
 
-function ShatteredSymbols:redTreasureForbiddenMind()
-    for playerIndex = 0, game:GetNumPlayers() - 1 do
-        local player = Isaac.GetPlayer(playerIndex)
-        if player:HasCollectible(ForbiddenMindLocalID) then
-            if math.random(100) <= 40 then
-                local level = game:GetLevel()
-                level:ChangeRoomType(RoomType.ROOM_RED_TREASURE)
-            end
-        end
-    end
-end
-
-
 ShatteredSymbols:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, ShatteredSymbols.onRoomClearForbiddenMind)
-ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, ShatteredSymbols.redTreasureForbiddenMind)
