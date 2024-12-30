be9a499cce444e98880753deba8f3827704b2a43
Luca Del Conte <delconteluca55@gmail.com>
2024-12-30 13:57:02 +0100

    Exchanged Main
    TEST FOR FIX SAVES PROBLEM

--- Shattered Symbols Compaq/main.lua
+++ Shattered Symbols Compaq/main.lua
@@ -64,110 +64,128 @@ end
 --------------------- Save Datas ---------------------
 
 function ShatteredSymbols:SavePlayerData()
-    local player = Isaac.GetPlayer(0)
-    local data = player:GetData()
+    local numPlayers = Game():GetNumPlayers()
+    local allPlayersData = {}
 
-    AllPlayerDataToSave = {
-        OrigamiSwanRelative = data.OrigamiSwanRelative or 0,
-        OrigamiSwanPreviousCounter = data.OrigamiSwanPreviousCounter or 1,
-        OrigamiShurikenRelative = data.OrigamiShurikenRelative or 0,
-        OrigamiShurikenPreviousCounter = data.OrigamiShurikenPreviousCounter or 1,
-        OrigamiShurikenDamageBoost = data.OrigamiShurikenDamageBoost or 0,
-        OrigamiBatRelative = data.OrigamiBatRelative or 0,
-        OrigamiBatPreviousCounter = data.OrigamiBatPreviousCounter or 1,
-        OrigamiBatTearsCount = data.OrigamiBatTearsCount or 0,
-        OrigamiBatSpeedBoost = data.OrigamiBatSpeedBoost or 0,
-        OrigamiBatLimit = data.OrigamiBatLimit or 0,
-        OrigamiKolibriChargeMemory = data.OrigamiKolibriChargeMemory or {},
-        OrigamiKolibriRelative = data.OrigamiKolibriRelative or 0,
-        OrigamiKolibriPreviousCounter = data.OrigamiKolibriPreviousCounter or 1,
-        OrigamiBoatBrokenHeartsCount = data.OrigamiBoatBrokenHeartsCount or 0,
-        OrigamiBoatHoldingItemforStats = data.OrigamiBoatHoldingItemforStats or false,
-        MeteorRelative = data.MeteorRelative or 0,
-        MeteorPreviousCounter = data.MeteorPreviousCounter or 1,
-        FortuneTellerRelative = data.FortuneTellerRelative or 0,
-        FortuneTellerPreviousCounter = data.FortuneTellerPreviousCounter or 1,
-        FortuneTellerLuckBoost = data.FortuneTellerLuckBoost or 0,
-        BrokenFluxPreviousBrokenHearts = data.BrokenFluxPreviousBrokenHearts or -1,
-        BrokenFluxCharge = data.BrokenFluxCharge or 0,
-        OrigamiCrowRelative = data.OrigamiCrowRelative or 0,
-        OrigamiCrowPreviousCounter = data.OrigamiCrowPreviousCounter or 1,
-        BrokenBoxHeartFlag = data.BrokenBoxHeartFlag or false,
-        BrokenBoxItemFlag = data.BrokenBoxItemFlag or nil,
-        BrokenBoxMoneyFlag = data.BrokenBoxMoneyFlag or 0,
-        BrokenBoxBombFlag = data.BrokenBoxBombFlag or 0,
-        BrokenBoxKeyFlag = data.BrokenBoxKeyFlag or 0,
-        BrokenBoxStatus = data.BrokenBoxStatus or false,
-        AncientHookCounter = data.AncientHookCounter or 0,
-        RunicAltarEffects = data.RunicAltarEffects or {},
-    }
+    for i = 0, numPlayers - 1 do
+        local player = Isaac.GetPlayer(i)
+        local data = player:GetData()
 
-    ShatteredSymbols:SaveData(JsonSaveFile.encode(AllPlayerDataToSave))
-    AllPlayerDataToSave = {}
+        local playerDataToSave = {
+            OrigamiSwanRelative = data.OrigamiSwanRelative or 0,
+            OrigamiSwanPreviousCounter = data.OrigamiSwanPreviousCounter or 1,
+            OrigamiShurikenRelative = data.OrigamiShurikenRelative or 0,
+            OrigamiShurikenPreviousCounter = data.OrigamiShurikenPreviousCounter or 1,
+            OrigamiShurikenDamageBoost = data.OrigamiShurikenDamageBoost or 0,
+            OrigamiBatRelative = data.OrigamiBatRelative or 0,
+            OrigamiBatPreviousCounter = data.OrigamiBatPreviousCounter or 1,
+            OrigamiBatTearsCount = data.OrigamiBatTearsCount or 0,
+            OrigamiBatSpeedBoost = data.OrigamiBatSpeedBoost or 0,
+            OrigamiBatLimit = data.OrigamiBatLimit or 0,
+            OrigamiKolibriChargeMemory = data.OrigamiKolibriChargeMemory or {},
+            OrigamiKolibriRelative = data.OrigamiKolibriRelative or 0,
+            OrigamiKolibriPreviousCounter = data.OrigamiKolibriPreviousCounter or 1,
+            OrigamiBoatBrokenHeartsCount = data.OrigamiBoatBrokenHeartsCount or 0,
+            OrigamiBoatHoldingItemforStats = data.OrigamiBoatHoldingItemforStats or false,
+            MeteorRelative = data.MeteorRelative or 0,
+            MeteorPreviousCounter = data.MeteorPreviousCounter or 1,
+            FortuneTellerRelative = data.FortuneTellerRelative or 0,
+            FortuneTellerPreviousCounter = data.FortuneTellerPreviousCounter or 1,
+            FortuneTellerLuckBoost = data.FortuneTellerLuckBoost or 0,
+            BrokenFluxPreviousBrokenHearts = data.BrokenFluxPreviousBrokenHearts or -1,
+            BrokenFluxCharge = data.BrokenFluxCharge or 0,
+            OrigamiCrowRelative = data.OrigamiCrowRelative or 0,
+            OrigamiCrowPreviousCounter = data.OrigamiCrowPreviousCounter or 1,
+            AncientHookCounter = data.AncientHookCounter or 0,
+            RunicAltarEffects = data.RunicAltarEffects or {},
+        }
 
+        -- Specifiche per il giocatore 0
+        if i == 0 then
+            playerDataToSave.BrokenBoxHeartFlag = data.BrokenBoxHeartFlag or false
+            playerDataToSave.BrokenBoxItemFlag = data.BrokenBoxItemFlag or nil
+            playerDataToSave.BrokenBoxMoneyFlag = data.BrokenBoxMoneyFlag or 0
+            playerDataToSave.BrokenBoxBombFlag = data.BrokenBoxBombFlag or 0
+            playerDataToSave.BrokenBoxKeyFlag = data.BrokenBoxKeyFlag or 0
+            playerDataToSave.BrokenBoxStatus = data.BrokenBoxStatus or false
+        end
+
+        table.insert(allPlayersData, playerDataToSave)
+    end
+
+    ShatteredSymbols:SaveData(JsonSaveFile.encode(allPlayersData))
 end
 
 --------------------- Load Saves ---------------------
 
 function ShatteredSymbols:LoadPlayerData()
-    local player = Isaac.GetPlayer(0)
-    local data = player:GetData()
+    local numPlayers = Game():GetNumPlayers()
 
     if ShatteredSymbols:HasData() and (Game():GetFrameCount() > 0) then
+        local allPlayersData = JsonSaveFile.decode(ShatteredSymbols:LoadData())
 
-        AllPlayerDataToSave = JsonSaveFile.decode(ShatteredSymbols:LoadData())
+        for i = 0, numPlayers - 1 do
+            local player = Isaac.GetPlayer(i)
+            local data = player:GetData()
+            local playerDataToLoad = allPlayersData[i + 1] or {}
 
-        data.OrigamiSwanRelative = AllPlayerDataToSave.OrigamiSwanRelative or 0
-        data.OrigamiSwanPreviousCounter = AllPlayerDataToSave.OrigamiSwanPreviousCounter or 1
-        data.OrigamiShurikenRelative = AllPlayerDataToSave.OrigamiShurikenRelative or 0
-        data.OrigamiShurikenPreviousCounter = AllPlayerDataToSave.OrigamiShurikenPreviousCounter or 1
-        data.OrigamiShurikenDamageBoost = AllPlayerDataToSave.OrigamiShurikenDamageBoost or 0
-        data.OrigamiBatRelative = AllPlayerDataToSave.OrigamiBatRelative or 0
-        data.OrigamiBatPreviousCounter = AllPlayerDataToSave.OrigamiBatPreviousCounter or 1
-        data.OrigamiBatTearsCount = AllPlayerDataToSave.OrigamiBatTearsCount or 0
-        data.OrigamiBatSpeedBoost = AllPlayerDataToSave.OrigamiBatSpeedBoost or 0
-        data.OrigamiBatLimit = AllPlayerDataToSave.OrigamiBatLimit or 0
-        data.OrigamiKolibriChargeMemory = AllPlayerDataToSave.OrigamiKolibriChargeMemory or {}
-        data.OrigamiKolibriRelative = AllPlayerDataToSave.OrigamiKolibriRelative or 0
-        data.OrigamiKolibriPreviousCounter = AllPlayerDataToSave.OrigamiKolibriPreviousCounter or 1
-        data.OrigamiBoatBrokenHeartsCount = AllPlayerDataToSave.OrigamiBoatBrokenHeartsCount or 0
-        data.OrigamiBoatHoldingItemforStats = AllPlayerDataToSave.OrigamiBoatHoldingItemforStats or false
-        data.MeteorRelative = AllPlayerDataToSave.MeteorRelative or 0
-        data.MeteorPreviousCounter = AllPlayerDataToSave.MeteorPreviousCounter or 1
-        data.FortuneTellerRelative = AllPlayerDataToSave.FortuneTellerRelative or 0
-        data.FortuneTellerPreviousCounter = AllPlayerDataToSave.FortuneTellerPreviousCounter or 1
-        data.FortuneTellerLuckBoost = AllPlayerDataToSave.FortuneTellerLuckBoost or 0
-        data.BrokenFluxPreviousBrokenHearts = AllPlayerDataToSave.BrokenFluxPreviousBrokenHearts or -1
-        data.BrokenFluxCharge = AllPlayerDataToSave.BrokenFluxCharge or 0
-        data.OrigamiCrowRelative = AllPlayerDataToSave.OrigamiCrowRelative or 0
-        data.OrigamiCrowPreviousCounter = AllPlayerDataToSave.OrigamiCrowPreviousCounter or 1
-        data.BrokenBoxHeartFlag = AllPlayerDataToSave.BrokenBoxHeartFlag or false
-        data.BrokenBoxItemFlag = AllPlayerDataToSave.BrokenBoxItemFlag or nil
-        data.BrokenBoxMoneyFlag = AllPlayerDataToSave.BrokenBoxMoneyFlag or 0
-        data.BrokenBoxBombFlag = AllPlayerDataToSave.BrokenBoxBombFlag or 0
-        data.BrokenBoxKeyFlag = AllPlayerDataToSave.BrokenBoxKeyFlag or 0
-        data.BrokenBoxStatus = AllPlayerDataToSave.BrokenBoxStatus or false
-        data.AncientHookCounter = AllPlayerDataToSave.AncientHookCounter or 0
-        data.RunicAltarEffects = AllPlayerDataToSave.RunicAltarEffects or {}
+            data.OrigamiSwanRelative = playerDataToLoad.OrigamiSwanRelative or 0
+            data.OrigamiSwanPreviousCounter = playerDataToLoad.OrigamiSwanPreviousCounter or 1
+            data.OrigamiShurikenRelative = playerDataToLoad.OrigamiShurikenRelative or 0
+            data.OrigamiShurikenPreviousCounter = playerDataToLoad.OrigamiShurikenPreviousCounter or 1
+            data.OrigamiShurikenDamageBoost = playerDataToLoad.OrigamiShurikenDamageBoost or 0
+            data.OrigamiBatRelative = playerDataToLoad.OrigamiBatRelative or 0
+            data.OrigamiBatPreviousCounter = playerDataToLoad.OrigamiBatPreviousCounter or 1
+            data.OrigamiBatTearsCount = playerDataToLoad.OrigamiBatTearsCount or 0
+            data.OrigamiBatSpeedBoost = playerDataToLoad.OrigamiBatSpeedBoost or 0
+            data.OrigamiBatLimit = playerDataToLoad.OrigamiBatLimit or 0
+            data.OrigamiKolibriChargeMemory = playerDataToLoad.OrigamiKolibriChargeMemory or {}
+            data.OrigamiKolibriRelative = playerDataToLoad.OrigamiKolibriRelative or 0
+            data.OrigamiKolibriPreviousCounter = playerDataToLoad.OrigamiKolibriPreviousCounter or 1
+            data.OrigamiBoatBrokenHeartsCount = playerDataToLoad.OrigamiBoatBrokenHeartsCount or 0
+            data.OrigamiBoatHoldingItemforStats = playerDataToLoad.OrigamiBoatHoldingItemforStats or false
+            data.MeteorRelative = playerDataToLoad.MeteorRelative or 0
+            data.MeteorPreviousCounter = playerDataToLoad.MeteorPreviousCounter or 1
+            data.FortuneTellerRelative = playerDataToLoad.FortuneTellerRelative or 0
+            data.FortuneTellerPreviousCounter = playerDataToLoad.FortuneTellerPreviousCounter or 1
+            data.FortuneTellerLuckBoost = playerDataToLoad.FortuneTellerLuckBoost or 0
+            data.BrokenFluxPreviousBrokenHearts = playerDataToLoad.BrokenFluxPreviousBrokenHearts or -1
+            data.BrokenFluxCharge = playerDataToLoad.BrokenFluxCharge or 0
+            data.OrigamiCrowRelative = playerDataToLoad.OrigamiCrowRelative or 0
+            data.OrigamiCrowPreviousCounter = playerDataToLoad.OrigamiCrowPreviousCounter or 1
+            data.AncientHookCounter = playerDataToLoad.AncientHookCounter or 0
+            data.RunicAltarEffects = playerDataToLoad.RunicAltarEffects or {}
+
+            -- Specifiche per il giocatore 0
+            if i == 0 then
+                data.BrokenBoxHeartFlag = playerDataToLoad.BrokenBoxHeartFlag or false
+                data.BrokenBoxItemFlag = playerDataToLoad.BrokenBoxItemFlag or nil
+                data.BrokenBoxMoneyFlag = playerDataToLoad.BrokenBoxMoneyFlag or 0
+                data.BrokenBoxBombFlag = playerDataToLoad.BrokenBoxBombFlag or 0
+                data.BrokenBoxKeyFlag = playerDataToLoad.BrokenBoxKeyFlag or 0
+                data.BrokenBoxStatus = playerDataToLoad.BrokenBoxStatus or false
+            end
+        end
 
     elseif ShatteredSymbols:HasData() and (Game():GetFrameCount() == 0) then
-        
-        AllPlayerDataToSave = JsonSaveFile.decode(ShatteredSymbols:LoadData())
+        local allPlayersData = JsonSaveFile.decode(ShatteredSymbols:LoadData())
+        local playerDataToLoad = allPlayersData[1] or {}
 
-        data.BrokenBoxHeartFlag = AllPlayerDataToSave.BrokenBoxHeartFlag or false
-        data.BrokenBoxItemFlag = AllPlayerDataToSave.BrokenBoxItemFlag or nil
-        data.BrokenBoxMoneyFlag = AllPlayerDataToSave.BrokenBoxMoneyFlag or 0
-        data.BrokenBoxBombFlag = AllPlayerDataToSave.BrokenBoxBombFlag or 0
-        data.BrokenBoxKeyFlag = AllPlayerDataToSave.BrokenBoxKeyFlag or 0
-        data.BrokenBoxStatus = AllPlayerDataToSave.BrokenBoxStatus or false
-        
+        local player = Isaac.GetPlayer(0)
+        local data = player:GetData()
+
+        data.BrokenBoxHeartFlag = playerDataToLoad.BrokenBoxHeartFlag or false
+        data.BrokenBoxItemFlag = playerDataToLoad.BrokenBoxItemFlag or nil
+        data.BrokenBoxMoneyFlag = playerDataToLoad.BrokenBoxMoneyFlag or 0
+        data.BrokenBoxBombFlag = playerDataToLoad.BrokenBoxBombFlag or 0
+        data.BrokenBoxKeyFlag = playerDataToLoad.BrokenBoxKeyFlag or 0
+        data.BrokenBoxStatus = playerDataToLoad.BrokenBoxStatus or false
+
     else
         ShatteredSymbols:RemoveData()
     end
-
-    AllPlayerDataToSave = {}
-    
 end
+
 
 --------------------- Console Command ---------------------
 
