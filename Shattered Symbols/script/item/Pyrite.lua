374f376dd732c0c3b174798cfcbbc3c6ea70327f
Luca Del Conte <delconteluca55@gmail.com>
2025-02-01 01:12:19 +0100

    Added Pyrite
    need to test it

--- Shattered Symbols/script/item/Pyrite.lua
+++ Shattered Symbols/script/item/Pyrite.lua
@@ -4,6 +4,32 @@ local PyriteLocalID = Isaac.GetItemIdByName("Pyrite")
 
 -- EID (se usi EID per la descrizione)
 if EID then
-    EID:addCollectible(PyriteLocalID, "{{Coin}} ")
+    EID:addCollectible(PyriteLocalID, "{{Coin}} All coins turn into Golden Pennies #{{ArrowDown}} Your coins reset to 0 at the start of each floor")
 end
 
+function ShatteredSymbols:OnPickupInitPyrite(entity)
+ 
+    if entity.Variant == PickupVariant.PICKUP_COIN then
+        for i = 0, game:GetNumPlayers() - 1 do
+            local player = Isaac.GetPlayer(i)  
+            if player:HasCollectible(PyriteLocalID) then
+                entity.SubType = CoinSubType.COIN_GOLDEN_PENNY
+            end
+        end
+    end
+end
+
+function ShatteredSymbols:OnNewLevelPyrite()
+    if player:HasCollectible(PyriteLocalID) then
+        for i = 0, game:GetNumPlayers() - 1 do
+            local player = Isaac.GetPlayer(i)
+            local currentCoins = player:GetNumCoins()
+            if currentCoins ~= 0 then
+                player:AddCoins(-currentCoins)
+            end
+        end
+    end 
+end
+
+ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, ShatteredSymbols.OnPickupInitPyrite, PickupVariant.PICKUP_COIN)
+ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, ShatteredSymbols.OnNewLevelPyrite)
