5d809b1a40813d1611b01fb65cfceb527fc64915
Luca Del Conte <delconteluca55@gmail.com>
2025-02-13 12:12:43 +0100

    Update UnstableGlyph.lua

--- Shattered Symbols/script/item/UnstableGlyph.lua
+++ Shattered Symbols/script/item/UnstableGlyph.lua
@@ -14,34 +14,41 @@ local function tablecontains(tbl, element)
     return false
 end
 
-function ShatteredSymbols:havingUnstableGlyph(pickup, collider)
+function ShatteredSymbols:passiveUnstableGlyph(pickup, collider)
+    local SetterData = Isaac.GetPlayer(0)
+    local data = SetterData:GetData()
+    if pickup.Variant == PickupVariant.PICKUP_HEART and pickup.SubType == HeartSubType.HEART_ETERNAL then
+        local haveUnstable = false
+        for playerIndex = 0, game:GetNumPlayers() - 1 do
+            local player = Isaac.GetPlayer(playerIndex)
+            if player:HasCollectible(UnstableGlyphLocalID) then
+                haveUnstable = true
+            end
+        end
+        if haveUnstable then
+            local playerCollider = collider:ToPlayer()
+            data.UnstableGlyphCharge = data.UnstableGlyphCharge + 1
+            playerCollider:AddEternalHearts(-1)
+            playerCollider:AddBrokenHearts(1)
+        end
+    end
+end
+
+function ShatteredSymbols:havingUnstableGlyph(player)
     local SetterData = Isaac.GetPlayer(0)
     local data = SetterData:GetData()
     if not data.UnstableGlyphCharge then data.UnstableGlyphCharge = 0 end
     
-    if pickup.Variant == PickupVariant.PICKUP_HEART and pickup.SubType == HeartSubType.HEART_ETERNAL then
-        local playerCollider = collider:ToPlayer()
-        for playerIndex = 0, game:GetNumPlayers() - 1 do
-            local player = Isaac.GetPlayer(playerIndex)
-            if player:HasCollectible(UnstableGlyphLocalID) then
+        if player:HasCollectible(UnstableGlyphLocalID) then
 
-                for i = 0, 3 do -- Check all active item slots
-                    local activeItem = player:GetActiveItem(i)
+            for i = 0, 3 do -- Check all active item slots
+                local activeItem = player:GetActiveItem(i)
 
-                    if activeItem ~= 0 and activeItem == UnstableGlyphLocalID then
-                        if data.UnstableGlyphCharge < 7 then
-                            data.UnstableGlyphCharge = data.UnstableGlyphCharge + 1
-                            playerCollider:AddBrokenHearts(1)
-                            playerCollider:AddEternalHearts(-1)
-                        end
-
-                        if data.UnstableGlyphCharge > 7 then
-                            data.UnstableGlyphCharge = 7
-                        end
-                        
-                        player:SetActiveCharge(data.UnstableGlyphCharge, i)
-                        
+                if activeItem ~= 0 and activeItem == UnstableGlyphLocalID then
+                    if data.UnstableGlyphCharge > 7 then
+                        data.UnstableGlyphCharge = 7
                     end
+                    player:SetActiveCharge(data.UnstableGlyphCharge, i)
                 end
             end
         end
@@ -58,7 +65,7 @@ function ShatteredSymbols:useUnstableGlyph(_, rng, player)
             ItemOverlay.Show(Isaac.GetGiantBookIdByName("Glyph"), 0 , player)
         end
 
-        Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.EXPLOSION_LARGE, 0, isaacPos, Vector.Zero, nil)
+        --Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.EXPLOSION_LARGE, 0, isaacPos, Vector.Zero, nil)
         --SFXManager():Play(SoundEffect.SOUND_MAMA_MEGA_BOOM)
 
         if itemConfig and itemConfig.Quality == 4 and not tablecontains(collectedItems, itemConfig.ID) and not tablecontains(itemIgnoreList, itemConfig.ID) then
@@ -120,7 +127,8 @@ function ShatteredSymbols:ClearListUnstableGlyph()
 end
 
 ShatteredSymbols:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, ShatteredSymbols.FluxWispInit, FamiliarVariant.WISP)
-ShatteredSymbols:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, ShatteredSymbols.havingUnstableGlyph)
+ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, ShatteredSymbols.havingUnstableGlyph)
+ShatteredSymbols:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, ShatteredSymbols.passiveUnstableGlyph)
 ShatteredSymbols:AddCallback(ModCallbacks.MC_USE_ITEM, ShatteredSymbols.useUnstableGlyph, UnstableGlyphLocalID)
 ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, ShatteredSymbols.AddItemToListUnstableGlyph)
 ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, ShatteredSymbols.ClearListUnstableGlyph)
