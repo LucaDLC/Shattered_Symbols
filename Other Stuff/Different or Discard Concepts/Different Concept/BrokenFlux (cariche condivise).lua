d1d7932386b1a7e0ded15e6a36f21c765e84000a
Luca Del Conte <delconteluca55@gmail.com>
2025-02-17 20:53:57 +0100

    Little Nerf

--- Other Stuff/Different or Discard Concepts/Different Concept/BrokenFlux (cariche condivise).lua
+++ Other Stuff/Different or Discard Concepts/Different Concept/BrokenFlux (cariche condivise).lua
@@ -3,37 +3,53 @@ local BrokenFluxLocalID = Isaac.GetItemIdByName("Broken Flux")
 local OrigamiCrowExternalID = Isaac.GetItemIdByName("Origami Crow")
 
 if EID then
-    EID:addCollectible(BrokenFluxLocalID, "{{Warning}} SINGLE USE {{Warning}} #{{UltraSecretRoom}} Teleport in Ultra Secret Room #{{BrokenHeart}} When you hold the item, after gaining Broken Heart, the item remove it for charging, every Broken Heart is equal to one charge #{{ArrowDown}} If the absorbed Broken Hearts have replaced Heart, these are not returned")
+    EID:addCollectible(BrokenFluxLocalID, "{{Warning}} SINGLE USE {{Warning}} #{{UltraSecretRoom}} Teleport in Ultra Secret Room #{{BrokenHeart}} When you hold the item, after gaining Broken Heart, the item remove it for charging, every Broken Heart is equal to one charge #{{ArrowDown}} If the absorbed Broken Hearts have replaced Heart, these are not returned #{{ArrowUp}} Broken Flux share charges with all Broken Flux of the same player during the game ")
 end
+
 
 function ShatteredSymbols:havingBrokenFlux(player)
     -- Get the player's data table
     local data = player:GetData()
     if not data.BrokenFluxPreviousBrokenHearts then data.BrokenFluxPreviousBrokenHearts = -1 end
+    if not data.BrokenFluxCharge then data.BrokenFluxCharge = 0 end
 
     if player:HasCollectible(BrokenFluxLocalID) then
+
         for i = 0, 3 do -- Check all active item slots
             local activeItem = player:GetActiveItem(i)
+
             if activeItem ~= 0 and activeItem == BrokenFluxLocalID then
+
                 local currentBrokenHearts = player:GetBrokenHearts()
+
                 if data.BrokenFluxPreviousBrokenHearts == -1 then
                     data.BrokenFluxPreviousBrokenHearts = currentBrokenHearts
                 end
+
                 
-                if currentBrokenHearts > data.BrokenFluxPreviousBrokenHearts then
+                if currentBrokenHearts > data.BrokenFluxPreviousBrokenHearts and player:HasCollectible(BrokenFluxLocalID) and data.BrokenFluxCharge < 3 then
                     for Diff = 1, (currentBrokenHearts - data.BrokenFluxPreviousBrokenHearts) do
-                        if player:GetActiveCharge(i) < 3 then
+                        if data.BrokenFluxCharge < 3 then
+                            data.BrokenFluxCharge = data.BrokenFluxCharge + 1
+                            if player:HasCollectible(OrigamiCrowExternalID) then
+                                data.BrokenFluxCharge = data.BrokenFluxCharge + 1
+                            end
                             player:AddBrokenHearts(-1)
-                            player:SetActiveCharge(player:GetActiveCharge(i) + 1, i)
-                        else  
-                            player:SetActiveCharge(3, i)
                         end
                     end
+                end
+
+                if player:HasCollectible(BrokenFluxLocalID) then
+                    if data.BrokenFluxCharge > 3 then
+                        data.BrokenFluxCharge = 3
+                    end
+                    player:SetActiveCharge(data.BrokenFluxCharge, i)
                 end
                 
                 data.BrokenFluxPreviousBrokenHearts = currentBrokenHearts
             end
         end
+
     else
         data.BrokenFluxPreviousBrokenHearts = -1
     end
@@ -42,6 +58,7 @@ end
 function ShatteredSymbols:useBrokenFlux(_, rng, player)
     local data = player:GetData()
     if player:HasCollectible(BrokenFluxLocalID) then
+
         local targetRoomIndex = getRoomIndexByType(RoomType.ROOM_ULTRASECRET)
         if targetRoomIndex then
             game:StartRoomTransition(targetRoomIndex, -1, RoomTransitionAnim.TELEPORT)
@@ -68,13 +85,14 @@ function getRoomIndexByType(roomType)
 end
 
 function ShatteredSymbols:FluxWispInit(wisp)
-    if wisp.Player and wisp.Player:HasCollectible(BrokenFluxLocalID) then
-        if wisp.SubType == BrokenFluxLocalID then
-            wisp.SubType = 580
-        end
-    end
+	if  wisp.Player and wisp.Player:HasCollectible(BrokenFluxLocalID) then
+		if wisp.SubType == BrokenFluxLocalID then
+			wisp.SubType = 580
+		end
+	end
 end
 
 ShatteredSymbols:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, ShatteredSymbols.FluxWispInit, FamiliarVariant.WISP)
 ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, ShatteredSymbols.havingBrokenFlux)
 ShatteredSymbols:AddCallback(ModCallbacks.MC_USE_ITEM, ShatteredSymbols.useBrokenFlux, BrokenFluxLocalID)
+
