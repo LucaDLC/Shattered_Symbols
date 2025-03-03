bc1664f0a9942ed362b22937376b1907b51616ad
Luca Del Conte <delconteluca55@gmail.com>
2025-03-03 12:13:11 +0100

    Update Pallas.lua

--- Shattered Symbols/script/item/Pallas.lua
+++ Shattered Symbols/script/item/Pallas.lua
@@ -3,11 +3,12 @@ local PallasLocalID = Isaac.GetItemIdByName("Pallas")
 
 -- EID (External Item Descriptions)
 if EID then
-    EID:addCollectible(PallasLocalID, "{{}} Occasionally, ")
+    EID:addCollectible(PallasLocalID, "{{}} Every room you have 5% of chance to substitute a not volatile Item with an Mutable Onyx #{{}} Occasionally, active effects of a rune #{{ArrowUp}} Both effects increasing with numbers of Vesta you have")
 end
 
 local function triggerPallas(player)
     
+    -- rune effects
     
 end
 
@@ -18,6 +19,10 @@ function ShatteredSymbols:PallasEffect(player)
 
         local numberOfPallass = player:GetCollectibleNum(PallasLocalID)
         if numberOfPallass > 0 then
+
+            --Take 1 Item that isn't inside the data.MutableOnyxItemEffectID table, is not Mutable Onyx and is not Pallas and substitute with a Mutable Onyx
+
+
             if numberOfPallass > 5 then numberOfPallass = 5 end
             local randomValue = math.random(1, math.floor(1024 / 2^numberOfPallass))
         
