05b2d1f0a65862d92e857f5ef4b3d2872f1ed017
Luca Del Conte <delconteluca55@gmail.com>
2025-04-02 02:06:59 +0200

    Buff Mask

--- Shattered Symbols/script/item/JadeIvoryMask.lua
+++ Shattered Symbols/script/item/JadeIvoryMask.lua
@@ -11,50 +11,47 @@ end
 
 -- EID (External Item Descriptions)
 if EID then
-    EID:addCollectible(JadeIvoryMaskLocalID, "{{Collectible}} On each floor, there is a 75% chance to upgrade a random item you have previously picked up (only items with quality 3 or lower) into a random item with quality increased by 1")
+    EID:addCollectible(JadeIvoryMaskLocalID, "{{Collectible}} On each floor, upgrade a random item you have previously picked up (only items with quality 3 or lower) into a random item with quality increased by 1")
 end
 
 function ShatteredSymbols:OnNewLevelJadeIvoryMask()
     for p = 0, game:GetNumPlayers() - 1 do
         local player = Isaac.GetPlayer(p)
         if player:HasCollectible(JadeIvoryMaskLocalID) then
-            if math.random() < 0.75 then
 
-                local eligibleItems = {}
+            local eligibleItems = {}
+            
+            for id = 1, Isaac.GetItemConfig():GetCollectibles().Size do
+                local itemConfig = Isaac.GetItemConfig():GetCollectible(id)
+                if itemConfig and id ~= JadeIvoryMaskLocalID and not itemIgnoreSet[id] and player:HasCollectible(id) then
+                    if itemConfig.Quality >= 0 and itemConfig.Quality <= 3 then
+                        table.insert(eligibleItems, id)
+                    end
+                end
+            end
+            
+            if #eligibleItems > 0 then
+                
+                local oldItemID = eligibleItems[math.random(1, #eligibleItems)]
+                local oldItemConfig = Isaac.GetItemConfig():GetCollectible(oldItemID)
+                local newQuality = oldItemConfig.Quality + 1
+                local pool = {}
                 
                 for id = 1, Isaac.GetItemConfig():GetCollectibles().Size do
                     local itemConfig = Isaac.GetItemConfig():GetCollectible(id)
-                    if itemConfig and id ~= JadeIvoryMaskLocalID and not itemIgnoreSet[id] and player:HasCollectible(id) then
-                        if itemConfig.Quality >= 0 and itemConfig.Quality <= 3 then
-                            table.insert(eligibleItems, id)
-                        end
+                    if itemConfig and not itemIgnoreSet[id] and itemConfig.Quality == newQuality then
+                        table.insert(pool, id)
                     end
                 end
 
-                if #eligibleItems > 0 then
+                if #pool > 0 then
+                    local newItemID = pool[math.random(1, #pool)]
                     
-                    local oldItemID = eligibleItems[math.random(1, #eligibleItems)]
-                    local oldItemConfig = Isaac.GetItemConfig():GetCollectible(oldItemID)
-                    local newQuality = oldItemConfig.Quality + 1
-
-                    local pool = {}
+                    player:RemoveCollectible(oldItemID)
+                    player:AddCollectible(newItemID, 0, false)
                     
-                    for id = 1, Isaac.GetItemConfig():GetCollectibles().Size do
-                        local itemConfig = Isaac.GetItemConfig():GetCollectible(id)
-                        if itemConfig and not itemIgnoreSet[id] and itemConfig.Quality == newQuality then
-                            table.insert(pool, id)
-                        end
-                    end
-
-                    if #pool > 0 then
-                        local newItemID = pool[math.random(1, #pool)]
-                        
-                        player:RemoveCollectible(oldItemID)
-                        player:AddCollectible(newItemID, 0, false)
-                        
-                        Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, player.Position, Vector(0,0), player)
-                        SFXManager():Play(SoundEffect.SOUND_1UP)
-                    end
+                    Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, player.Position, Vector(0,0), player)
+                    SFXManager():Play(SoundEffect.SOUND_1UP)
                 end
             end
         end
