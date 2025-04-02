99b98c5032a65369386b1dcce334ddf47072f198
Luca Del Conte <delconteluca55@gmail.com>
2025-04-02 13:31:14 +0200

    Mantle have Fly
    need to fix animation

--- Shattered Symbols/script/item/DystopicMantle.lua
+++ Shattered Symbols/script/item/DystopicMantle.lua
@@ -11,10 +11,19 @@ function ShatteredSymbols:useDystopicMantle()
     for i = 0, Game():GetNumPlayers() - 1 do
         local player = Isaac.GetPlayer(i)
         if player:IsDead() and player:HasCollectible(DystopicMantleLocalID) then
+            player:AddCacheFlags(CacheFlag.CACHE_FLYING)
+            player:EvaluateItems()
             player:RemoveCollectible(DystopicMantleLocalID)
             player:AddCollectible(CollectibleType.COLLECTIBLE_HOLY_MANTLE, 1, false)
         end
     end
 end
 
+function ShatteredSymbols:onEvaluateCacheDystopicMantle(player, cacheFlag)
+    if cacheFlag == CacheFlag.CACHE_FLYING and player:IsDead() and player:HasCollectible(DystopicMantleLocalID) then
+        player.CanFly = true
+    end
+end
+
 ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_UPDATE, ShatteredSymbols.useDystopicMantle)
+ShatteredSymbols:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, ShatteredSymbols.onEvaluateCacheDystopicMantle)
