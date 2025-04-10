8e2aaacf12b9b9537ff8b94ebeec7bf4b1c5045d
Luca Del Conte <delconteluca55@gmail.com>
2025-04-10 21:19:04 +0200

    Beta Test

---
+++ Other Stuff/ForbiddenBody.lua
@@ -0,0 +1,72 @@
+local game = Game()
+local ForbiddenBodyLocalID = Isaac.GetItemIdByName("Forbidden Body")
+
+-- EID (External Item Descriptions)
+if EID then
+    EID:addCollectible(ForbiddenBodyLocalID, "{{EthernalHeart}} Half Eternal Heart count as 2 and can remove 1 Broken Heart #{{BossRoom}} When you enter in the Boss Room for the first time you have 20% to obtain an Half Eternal Heart #{{Player10}} Half Eternal Heart count as Holy Card effect")
+end
+
+function ShatteredSymbols:useForbidenBody(player)
+    local data = player:GetData()
+    if not data.ForbiddenBodyMantleCounter then data.ForbiddenBodyMantleCounter = 0 end
+    if player:GetEternalHearts() == 1 and player:HasCollectible(ForbiddenBodyLocalID) then
+        local playerType = player:GetPlayerType()
+        if playerType == PlayerType.PLAYER_THELOST or playerType == PlayerType.PLAYER_THELOST_B then
+            player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_HOLY_MANTLE, true, 1)
+            data.ForbiddenBodyMantleCounter = data.ForbiddenBodyMantleCounter + 1
+        else
+            player:AddBrokenHearts(-1) 
+            player:AddEternalHearts(1) 
+        end
+    end
+    if player:GetEffects():GetCollectibleEffectNum(CollectibleType.COLLECTIBLE_HOLY_MANTLE) < data.ForbiddenBodyMantleCounter + 1 and playerType == PlayerType.PLAYER_THELOST or playerType == PlayerType.PLAYER_THELOST_B then
+        data.ForbiddenBodyMantleCounter = data.ForbiddenBodyMantleCounter - 1
+    end
+
+end  
+
+function ShatteredSymbols:holdingForbidenBody(pickup, collider)
+    if collider:ToPlayer() then
+        local player = collider:ToPlayer()
+        local playerType = player:GetPlayerType()
+        local data = player:GetData()
+        if pickup.Variant == PickupVariant.PICKUP_HEART and pickup.SubType == HeartSubType.HEART_ETERNAL and player:HasCollectible(ForbiddenBodyLocalID) then
+            if playerType == PlayerType.PLAYER_THELOST or playerType == PlayerType.PLAYER_THELOST_B then
+                player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_HOLY_MANTLE, true, 1)
+                data.ForbiddenBodyMantleCounter = data.ForbiddenBodyMantleCounter + 1
+            else
+                player:AddBrokenHearts(-1) 
+                player:AddEternalHearts(1)
+            end 
+        end
+    end
+end
+
+function ShatteredSymbols:onBossRoomForbidenBody()
+    local room = game:GetRoom()
+
+    for playerIndex = 0, game:GetNumPlayers() - 1 do
+        local player = Isaac.GetPlayer(playerIndex)
+        local data = player:GetData()
+        if data.ForbiddenBodyMantleCounter > 0 then
+            for i = 0, data.ForbiddenBodyMantleCounter do
+                player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_HOLY_MANTLE, true, 1)
+            end
+        end
+        if room:GetType() == RoomType.ROOM_BOSS and room:IsFirstVisit() and player:HasCollectible(ForbiddenBodyLocalID) then
+            local playerType = player:GetPlayerType()
+            if math.random() <= 0.20 then 
+                if playerType == PlayerType.PLAYER_THELOST or playerType == PlayerType.PLAYER_THELOST_B then
+                    player:GetEffects():AddCollectibleEffect(CollectibleType.COLLECTIBLE_HOLY_MANTLE, true, 1)
+                    data.ForbiddenBodyMantleCounter = data.ForbiddenBodyMantleCounter + 1
+                else
+                    player:AddEternalHearts(1) 
+                end
+            end
+        end
+    end
+end
+
+ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, ShatteredSymbols.useForbidenBody)
+ShatteredSymbols:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, ShatteredSymbols.holdingForbidenBody)
+ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, ShatteredSymbols.onBossRoomForbidenBody)
