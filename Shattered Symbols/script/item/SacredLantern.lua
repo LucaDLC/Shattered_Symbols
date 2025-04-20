e32f2f780b35abcf6ff29a448030cae4d2ad4fb9
Luca Del Conte <delconteluca55@gmail.com>
2025-04-20 23:24:56 +0200

    Buff Sacred Lantern

--- Shattered Symbols/script/item/SacredLantern.lua
+++ Shattered Symbols/script/item/SacredLantern.lua
@@ -3,7 +3,7 @@ local SacredLanternLocalID = Isaac.GetItemIdByName("Sacred Lantern")
 
 -- EID (External Item Descriptions)
 if EID then
-    EID:addCollectible(SacredLanternLocalID, "{{BrokenHeart}} Remove all your Broken Hearts #{{HalfSoulHeart}} For every Broken Heart removed you obtain an Half Soul Heart #{{Player14}} For every Broken Heart removed you obtain an Empty Coin Heart")
+    EID:addCollectible(SacredLanternLocalID, "{{BrokenHeart}} Remove all your Broken Hearts #{{SoulHeart}} For every Broken Heart removed you obtain an Soul Heart #{{Player14}} For every Broken Heart removed you obtain an Empty Coin Heart")
 end
 
 function ShatteredSymbols:useSacredLantern(_, rng, player)
@@ -17,7 +17,7 @@ function ShatteredSymbols:useSacredLantern(_, rng, player)
                 if (playerType == PlayerType.PLAYER_KEEPER or playerType == PlayerType.PLAYER_KEEPER_B) then
                     player:AddMaxHearts(2)
                 else
-                    player:AddSoulHearts(1) 
+                    player:AddSoulHearts(2) 
                 end         
             end
         end
