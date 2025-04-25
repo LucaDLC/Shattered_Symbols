58e8ae24f3a1bbe51ad75e6fc431b0c8a8ccd19b
Luca Del Conte <delconteluca55@gmail.com>
2025-04-25 17:07:17 +0200

    Fix Dystopic Mantle

--- Shattered Symbols/script/item/DystopicMantle.lua
+++ Shattered Symbols/script/item/DystopicMantle.lua
@@ -10,19 +10,30 @@ end
 function ShatteredSymbols:useDystopicMantle()
     for i = 0, Game():GetNumPlayers() - 1 do
         local player = Isaac.GetPlayer(i)
-        if player:IsDead() and player:HasCollectible(DystopicMantleLocalID) then
+        local data = player:GetData()
+        if not data.IsDeadDystopicMantle then data.IsDeadDystopicMantle = 5 end
+        if player:IsDead() and player:HasCollectible(DystopicMantleLocalID) then  
+            data.IsDeadDystopicMantle = 0
+        end
+        if not player:IsDead() and (data.IsDeadDystopicMantle < 5) and player:HasCollectible(DystopicMantleLocalID) then
             player:AddCacheFlags(CacheFlag.CACHE_FLYING)
             player:EvaluateItems()
+            data.IsDeadDystopicMantle = 10
             player:RemoveCollectible(DystopicMantleLocalID)
             player:AddCollectible(CollectibleType.COLLECTIBLE_HOLY_MANTLE, 1, false)
-            player:AddCostume(Isaac.GetItemConfig():GetCollectible(33), 1) -- non va
             SFXManager():Play(SoundEffect.SOUND_HOLY)
+        end
+        if data.IsDeadDystopicMantle > 5 then
+            data.IsDeadDystopicMantle = data.IsDeadDystopicMantle - 1 
+            player:AddCostume(Isaac.GetItemConfig():GetCollectible(33), 1)
         end
     end
 end
 
 function ShatteredSymbols:onEvaluateCacheDystopicMantle(player, cacheFlag)
-    if cacheFlag == CacheFlag.CACHE_FLYING and player:IsDead() and player:HasCollectible(DystopicMantleLocalID) then
+    local data = player:GetData()
+    if not data.IsDeadDystopicMantle then data.IsDeadDystopicMantle = 5 end
+    if cacheFlag == CacheFlag.CACHE_FLYING and (data.IsDeadDystopicMantle < 5) and player:HasCollectible(DystopicMantleLocalID) then
         player.CanFly = true
     end
 end
