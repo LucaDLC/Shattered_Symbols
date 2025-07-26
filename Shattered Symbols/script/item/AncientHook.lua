fab8ea213d08f6c155db81d7d03555c55affcb4f
Luca Del Conte <delconteluca55@gmail.com>
2025-07-27 01:17:20 +0200

    Improved & Little Rework Ancient Hook

--- Shattered Symbols/script/item/AncientHook.lua
+++ Shattered Symbols/script/item/AncientHook.lua
@@ -1,10 +1,11 @@
 
 local game = Game()
 local AncientHookLocalID = Isaac.GetItemIdByName("Ancient Hook")
+local globalHookTimer = nil
 
 -- EID (External Item Descriptions)
 if EID then
-    EID:addCollectible(AncientHookLocalID, "{{BrokenHeart}} Gives 1 Broken Hearts at every Floor which does not replace Heart until all available slots for new Hearts are used #{{Collectible628}} Every floor you have 25% of chance for each Ancient Hook of spawn Death Certificate #{{LuckSmall}} You have same Chance as Luck to remove Ancient Hooks on each floor, at the floor when Ancient Hooks removed the effects not activate")
+    EID:addCollectible(AncientHookLocalID, "{{BrokenHeart}} Gives 1 Broken Hearts at every Floor which does not replace Heart until all available slots for new Hearts are used #{{Collectible628}} First time you visit a room you have 3% of chance for each Ancient Hook of enter in Death Certificate room #{{LuckSmall}} You have same Chance as Luck to remove Ancient Hooks on each floor, at the floor when Ancient Hooks removed the effects not activate")
 end
 
 function ShatteredSymbols:onAncientHook()
@@ -20,23 +21,44 @@ function ShatteredSymbols:onAncientHook()
             else
                 local AncientHooksCounter = player:GetCollectibleNum(AncientHookLocalID)
                 player:AddBrokenHearts(1*AncientHooksCounter)
-                local AncientHookChance = 0
-                AncientHookChance = AncientHooksCounter * 0.25
-                if math.random() < AncientHookChance then
-                    local pocketItem = player:GetActiveItem(ActiveSlot.SLOT_POCKET)
-                    local activeItem = player:GetActiveItem(ActiveSlot.SLOT_PRIMARY)
-                    if pocketItem == 0 then
-                        player:SetPocketActiveItem(CollectibleType.COLLECTIBLE_DEATH_CERTIFICATE, ActiveSlot.SLOT_POCKET)
-                    elseif activeItem == 0 then
-                        player:AddCollectible(CollectibleType.COLLECTIBLE_DEATH_CERTIFICATE, 1, false)
-                    else
-                        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, CollectibleType.COLLECTIBLE_DEATH_CERTIFICATE, player.Position + Vector(0, 50), Vector(0,0), nil)
-                    end
-                    SFXManager():Play(SoundEffect.SOUND_THUMBSUP)
-                end
             end
         end
     end
 end
 
+function ShatteredSymbols:useAncientHook()
+    for playerIndex = 0, game:GetNumPlayers() - 1 do
+        local player = Isaac.GetPlayer(playerIndex)
+        local level = game:GetLevel()
+        local room = level:GetCurrentRoom()
+        local AncientHooksCounter = player:GetCollectibleNum(AncientHookLocalID)
+        if player:HasCollectible(AncientHookLocalID) then
+            local AncientHookChance = 0
+            AncientHookChance = AncientHooksCounter * 0.03
+            if math.random() < AncientHookChance and room:IsFirstVisit() then
+                globalHookTimer = 10
+            end
+        end
+    end
+
+end
+
+function ShatteredSymbols:updateAncientHook()
+    if globalHookTimer then
+        globalHookTimer = globalHookTimer - 1
+        if globalHookTimer <= 0 then
+            for playerIndex = 0, game:GetNumPlayers() - 1 do
+                local player = Isaac.GetPlayer(playerIndex)
+                if player:HasCollectible(AncientHookLocalID) then
+                    player:UseActiveItem(CollectibleType.COLLECTIBLE_DEATH_CERTIFICATE, UseFlag.USE_NOANIM)
+                end
+            end
+            globalHookTimer = nil
+        end
+    end
+end
+
+
 ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, ShatteredSymbols.onAncientHook)
+ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, ShatteredSymbols.useAncientHook)
+ShatteredSymbols:AddCallback(ModCallbacks.MC_POST_UPDATE, ShatteredSymbols.updateAncientHook)
